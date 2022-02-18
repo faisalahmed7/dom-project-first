baada4959486e53ca37b4ad93f79f683ff0cfcd5
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-02-18 18:53:36 +0600

    Expensecalculation

--- js/calculation.js
+++ js/calculation.js
@@ -1,31 +1,23 @@
-// Calculate your Expense from your inncome.
-function expenseCalculate() {
-    var income, foodcost, houserent, myClothes, expense;
-    income = Number(document.getElementById("income").value);
-    foodcost = Number(document.getElementById("food").value);
-    houserent = Number(document.getElementById("rent").value);
-    myClothes = Number(document.getElementById("clothes").value);
-    expense = foodcost + houserent + myClothes;
-    myBalance = income - expense;
-    document.getElementById("totalExpense").value = expense;
-    document.getElementById("balance").value = myBalance;
+// Calculating Expense 
+function totalExpenseCalculate() {
+    const incomeInput = Number(document.getElementById("income-field").value);
+    const foodcostInput = Number(document.getElementById("food-field").value);
+    const houserentInput = Number(document.getElementById("rent-field").value);
+    const myClothesInput = Number(document.getElementById("clothes-field").value);
+    let expense = foodcostInput + houserentInput + myClothesInput;
+    let myBalance = incomeInput - expense;
+    document.getElementById("expense-field").value = expense;
+    document.getElementById("balance-field").value = myBalance;
+
+
+    income.value = '';
+
 }
 
-//Save your Money 
 
-function saveMoney() {
+document.getElementById('expense-btn').addEventListener('click', function () {
+    totalExpenseCalculate();
 
-    // Calculate the percent ammount from your income.
-    var percent = document.getElementById("percent").value;
-    var num = document.getElementById("income").value;
-    document.getElementById("save-amount")
-        .value = (num / 100) * percent;
 
-    // Calculate the Total remain Balance of your Mini Bank.
-
-    var previousBalance = document.getElementById("balance").value;
-    var saveBalance = document.getElementById("save-amount").value;
-    document.getElementById("remainBalance")
-        .value = previousBalance - saveBalance;
-}
+})
 
