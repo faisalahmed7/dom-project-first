1435f82b3b5b08068b1174b4d355b881f462b2c4
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-02-18 23:02:17 +0600

    Final Edit

--- js/calculation.js
+++ js/calculation.js
@@ -1,4 +1,4 @@
-// Calculating Expense 
+/* Calculating Expense  */
 function totalExpenseCalculate() {
     const incomeInput = Number(document.getElementById("income-field").value);
     const foodcostInput = Number(document.getElementById("food-field").value);
@@ -10,27 +10,27 @@ function totalExpenseCalculate() {
     document.getElementById('balance-field').value = myRemainingBalance;
 
 }
-
+/* Handled by Calculate button */
 
 document.getElementById('expense-btn').addEventListener('click', function () {
     totalExpenseCalculate();
-
-
 })
-//Savings From Income 
+
+
+/* Savings From Income */
 
 function saveAmount() {
     const percentCalculate = document.getElementById("percent-field").value;
     const amount = document.getElementById("income-field").value;
     document.getElementById('savings-field').value = (amount / 100) * percentCalculate;
 
-    // Calculate the Total remain Balance of your Monthly Expense.
+    /* Calculate Remaining Expense. */
 
     const previousBalance = document.getElementById("balance-field").value;
     const saveBalance = document.getElementById("savings-field").value;
     document.getElementById('remaining-field').value = previousBalance - saveBalance;
 }
-
+/* Handled by Save button */
 document.getElementById('save-btn').addEventListener('click', function () {
     saveAmount();
 })
